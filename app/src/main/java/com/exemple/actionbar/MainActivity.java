644fa90a1c3e1e4e2dd67afc32880685ae9893d8
ana-valdivia <ana.raquel1291@hotmial.com>
2019-09-30 20:34:26 -0300

    Tela 2 Menu

--- app/src/main/java/com/exemple/actionbar/MainActivity.java
+++ app/src/main/java/com/exemple/actionbar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.exemple.actionbar;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onProximo(View view){
+        Intent intent = new Intent (this,ActionBarActivity.class);
+        startActivity(intent);
+
+       }
+
+
 }
+
+
