66b002ce3f299e0c723a259194c03beebe8c0aa6
ana-valdivia <ana.raquel1291@hotmial.com>
2019-09-30 22:29:13 -0300

    Acoes dos botoes

--- app/src/main/java/com/exemple/actionbar/ActionBarActivity.java
+++ app/src/main/java/com/exemple/actionbar/ActionBarActivity.java
@@ -2,12 +2,58 @@ package com.exemple.actionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.actions,menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_wifi:
+                Toast.makeText(this,"wi-fi acionado!",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_bluetooth:
+                Toast.makeText(this,"Bluetooth acionado!",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_sobre:
+                Toast.makeText(this,"Sobre acionado!",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.actio_sair:
+                finish();
+                return true;
+
+           default:
+           return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+
+        Toolbar myToolbar =findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
     }
+
+
 }
